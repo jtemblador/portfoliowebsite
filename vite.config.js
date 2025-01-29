da588376208ac23eeb3aed7e7acf121fdc26165e
j0e <j0e@T480s.thinkpad>
2025-01-28 21:59:00 -0800

    added dist folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+
+  },
+
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets'
+    }
+  },
+
   server: {
     host: '0.0.0.0', // Allow connections from any device on the local network
     port: 5174, // Optional: Make sure this matches the port you're using
